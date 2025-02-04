4288b902f7762cfc6c60b4b557087c3461676045
Takenskii <asskarovt22@gmail.com>
2025-02-04 15:16:56 +0500

    Managed Django Admin panel with security  considerations

--- expensetracker/settings.py
+++ expensetracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8h6vp3)p!^+x7i(jt)-5s!ao&@xmbjaf05c@rkm$m8j7grqo!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
